487d187d33c994b4743ebf18e290d014afabac11
minhganther <minh.ganther@gmail.com>
2021-11-05 13:09:59 +0100

    added comment to solution

--- python/darts/darts.py
+++ python/darts/darts.py
@@ -1,5 +1,9 @@
 def score(x, y):
 
+    """ Determine whether a dart with the coordinates x,y is contained within a     certain concentric circle and give points according to that.
+
+    The solution is based on the pythagorean equation a^2 + b^2 = c^2 with x and    y being the sides of the triangle and r the hypothenuse.
+    """
     coord_val = x**2 + y**2
     if coord_val <= 1:
         return 10
