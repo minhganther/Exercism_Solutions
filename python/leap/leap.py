5478476ec338f431b9a0a18dcdd6dc99f13c9fbe
minhganther <minh.ganther@gmail.com>
2021-11-05 13:15:51 +0100

    added leap solution

--- python/leap/leap.py
+++ python/leap/leap.py
@@ -1,2 +1,9 @@
 def leap_year(year):
-    pass
+    """Given a year, report if it is a leap year
+    """
+    
+    divisible_by_4 = year % 4 == 0
+    divisible_by_100 = year % 100 == 0
+    divisible_by_400 = year % 400 == 0
+
+    return divisible_by_4 and (not divisible_by_100 or divisible_by_400)
