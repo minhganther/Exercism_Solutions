1ad901a9c2c731f81b501e05b889e6f86b1d5f50
minhganther <minh.ganther@gmail.com>
2021-11-05 13:06:41 +0100

    added darts solution

--- python/darts/darts.py
+++ python/darts/darts.py
@@ -1,2 +1,10 @@
 def score(x, y):
-    pass
+
+    coord_val = x**2 + y**2
+    if coord_val <= 1:
+        return 10
+    elif coord_val <= 25:
+        return 5
+    elif coord_val <= 100:
+        return 1
+    return 0
