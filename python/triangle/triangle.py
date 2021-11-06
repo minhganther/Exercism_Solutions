7ab403c0b5abe0028a2bfa71bd1d46a3aeb6877d
minhganther <minh.ganther@gmail.com>
2021-11-06 23:15:45 +0100

    added solution triangle

--- python/triangle/triangle.py
+++ python/triangle/triangle.py
@@ -1,10 +1,15 @@
+"""Determine the type of a triangle"""
+
 def equilateral(sides):
-    pass
+    sides.sort()
+    return sides[0] == sides[1]  == sides[2] and 0 not in sides and sides[0]+sides[1]>sides[2]
 
 
 def isosceles(sides):
-    pass
+    sides.sort()
+    return (sides[0] == sides[1] or sides[0] == sides[2] or sides[1] == sides[2]) and 0 not in sides and sides[0]+sides[1]>sides[2]
 
 
 def scalene(sides):
-    pass
+    sides.sort()
+    return not equilateral(sides) and not isosceles(sides) and sides[0]+sides[1]>sides[2]
