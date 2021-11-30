82c8c3da8f489a5f735dd474dd231eb98ff3ebe6
minhganther <minh.ganther@gmail.com>
2021-11-30 17:45:10 +0100

    added solution yacht

--- python/yacht/yacht.py
+++ python/yacht/yacht.py
@@ -14,19 +14,19 @@ You can learn more here: https://en.wikipedia.org/wiki/Enumerated_type
 
 # Score categories.
 # Change the values as you see fit.
-YACHT = None
-ONES = None
-TWOS = None
-THREES = None
-FOURS = None
-FIVES = None
-SIXES = None
-FULL_HOUSE = None
-FOUR_OF_A_KIND = None
-LITTLE_STRAIGHT = None
-BIG_STRAIGHT = None
-CHOICE = None
+YACHT = lambda dice: 50 if len(set(dice)) == 1 else 0
+ONES = lambda dice: dice.count(1)
+TWOS = lambda dice: dice.count(2)*2  
+THREES = lambda dice: dice.count(3)*3
+FOURS = lambda dice: dice.count(4)*4
+FIVES = lambda dice: dice.count(5)*5
+SIXES = lambda dice: dice.count(6)*6
+FULL_HOUSE = lambda dice: sum(dice) if len(set(dice)) == 2 and dice.count(dice[0]) >= 2 else 0 
+FOUR_OF_A_KIND = lambda dice: sum(i*4 for i in set(dice) if dice.count(i) >= 4)
+LITTLE_STRAIGHT = lambda dice: 30 if sorted(set(dice)) == [1,2,3,4,5] else 0
+BIG_STRAIGHT = lambda dice: 30 if sorted(set(dice)) == [2,3,4,5,6] else 0
+CHOICE = lambda dice: sum(dice)
 
 
 def score(dice, category):
-    pass
+    return category(dice)
