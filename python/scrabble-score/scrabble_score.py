7fabc1f82028ffc07d729c82bf92f982dd96253b
minhganther <minh.ganther@gmail.com>
2021-12-03 16:14:23 +0100

    added solution scrabble

--- python/scrabble-score/scrabble_score.py
+++ python/scrabble-score/scrabble_score.py
@@ -1,2 +1,9 @@
+SCORES = {"AEIOULNRST": 1, "DG": 2, "BCMP": 3, "FHVWY": 4, "K": 5, "JX": 8, "QZ": 10}
+
 def score(word):
-    pass
+    score = 0
+    for letter in word.upper():
+        for key in SCORES.keys():
+            if letter in key:
+                score = score + SCORES.get(key)
+    return score
