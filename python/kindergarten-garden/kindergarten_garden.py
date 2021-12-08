e3cbe0f4417d8811fa594e974bea8325f2a7d8d0
minhganther <minh.ganther@gmail.com>
2021-12-08 16:29:46 +0100

    added solution kindergarden

--- python/kindergarten-garden/kindergarten_garden.py
+++ python/kindergarten-garden/kindergarten_garden.py
@@ -1,3 +1,34 @@
 class Garden:
-    def __init__(self, diagram, students):
-        pass
+    PLANTS = {'C': 'Clover',
+              'G': 'Grass',
+              'R': 'Radishes',
+              'V': 'Violets'}
+
+    STUDENTS = [
+        'Alice',
+        'Bob',
+        'Charlie',
+        'David',
+        'Eve',
+        'Fred',
+        'Ginny',
+        'Harriet',
+        'Ileana',
+        'Joseph',
+        'Kincaid',
+        'Larry',
+    ]
+
+    def __init__(self, diagram, students=STUDENTS):
+        self.diagram = diagram
+        self.students = sorted(students)
+        self.rows = diagram.split('\n')
+    
+    def plants(self, student):
+        col = self.students.index(student) * 2
+        plants = [self.rows[0][col], self.rows[0][col+1], 
+                  self.rows[1][col], self.rows[1][col+1]]
+
+        names_plants = [Garden.PLANTS[plant] for plant in plants]
+        
+        return names_plants
